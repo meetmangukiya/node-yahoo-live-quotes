85a59f860712e36c3b83d4f2b058d9b695dfd451
Meet Mangukiya <meetmangukiya98@gmail.com>
2020-01-25 20:40:47 +0530

    index.js: Add function that gets live quotes

---
+++ index.js
@@ -0,0 +1,21 @@
+const WebSocket = require("ws");
+const protobuf = require("protobufjs");
+require("./__finStreamer-proto")(protobuf);
+
+const getQuotesFor = (symbols, cb) => {
+  const ws = new WebSocket("wss://streamer.finance.yahoo.com/");
+
+  ws.on("open", () => {
+    console.log(`subscribing to ${symbols}`);
+    ws.send(JSON.stringify({ subscribe: symbols }));
+  });
+
+  ws.on("message", data => {
+    const PricingData = protobuf.roots.default.quotefeeder.PricingData;
+    const decodedData = PricingData.decode(Buffer.from(data, "base64"));
+    const decodedObject = PricingData.toObject(decodedData);
+    cb(decodedObject);
+  });
+};
+
+module.exports = { getQuotesFor };
