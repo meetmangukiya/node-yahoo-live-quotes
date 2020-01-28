78ca2c9144eadeff55ac285e9f0ba96739549a12
Meet Mangukiya <meetmangukiya98@gmail.com>
2020-01-29 01:35:31 +0530

    Use ArrayBuffer in browser

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const WebSocket = require("isomorphic-ws");
 const protobuf = require("protobufjs");
 require("./__finStreamer-proto")(protobuf);
 
+const base64ToArray = (base64) => {
+  var binaryString = atob(base64);
+  var len = binaryString.length;
+  var bytes = new Uint8Array(len);
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+};
+
 const getQuotesFor = (symbols, cb) => {
   const ws = new WebSocket("wss://streamer.finance.yahoo.com/");
 
@@ -12,8 +22,13 @@ const getQuotesFor = (symbols, cb) => {
 
   ws.onmessage = data => {
     const PricingData = protobuf.roots.default.quotefeeder.PricingData;
-    const decodedData = PricingData.decode(Buffer.from(data, "base64"));
-    const decodedObject = PricingData.toObject(decodedData);
+    let decodedObject = null;
+
+    if (typeof process === 'object')
+      decodedData = PricingData.decode(Buffer.from(data, "base64"));
+    else
+      decodedData = PricingData.decode(base64ToArray(data));
+    decodedObject = PricingData.toObject(decodedData);
     cb(decodedObject);
   };
 
